d40b3d8f824f0592821d3a7113ada7d944481dd0
Vishal <vishal@Vishals-MacBook-Air.local>
2023-07-27 12:40:31 +0530

    connecting db and adding schema

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ const userRoutes = require("./routes/userRoutes");
 const buyerRoutes = require("./routes/buyerRoutes");
 const sellerRoutes = require("./routes/sellerRoutes");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://root:root@cluster0.mwczlh4.mongodb.net/?retryWrites=true&w=majority")
+.then(() => {
+    app.listen(5000, () => {
+        console.log(' serving server');
+    });
+})
+.catch((error) => {
+    console.log(error);
+})
 
 app.use("/api/auth", userRoutes);
 app.use("/api/buyer", buyerRoutes);
@@ -10,8 +21,4 @@ app.use("/api/seller", sellerRoutes);
 
 app.get("/", (req, res) => {
     res.send("Hey");
-});
-
-app.listen(5000, () => {
-    console.log(' serving server');
 });
